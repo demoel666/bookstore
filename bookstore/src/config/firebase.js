931b2b7228c3e412ddcde308347f7be820c6ecec
dannyknd7@gmail.com <dannyknd7@gmail.com>
2018-04-21 16:30:55 -0500

    fin del cuerso por hoy

---
+++ bookstore/src/config/firebase.js
@@ -0,0 +1,8 @@
+export default{
+    apiKey: "AIzaSyCNVH20mk0EolwByT-cnudlkF_lX2FlLZM",
+    authDomain: "bookstore-d0c4f.firebaseapp.com",
+    databaseURL: "https://bookstore-d0c4f.firebaseio.com",
+    projectId: "bookstore-d0c4f",
+    storageBucket: "bookstore-d0c4f.appspot.com",
+    messagingSenderId: "50264801684"
+};
